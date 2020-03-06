09592e1fdd2bc4f844ca3a8bf88a25fa46321106
Wesley Wai <wesley.wai@gmail.com>
2020-03-06 14:54:57 +0800

    Create toUpperAll.js

---
+++ toUpperAll.js
@@ -0,0 +1,10 @@
+const toUpperAll = str => {
+    str = str.split(" ");
+
+    for (var i = 0, x = str.length; i < x; i++) {
+        str[i] = str[i][0].toUpperCase() + str[i].substr(1);
+    }
+
+    return str.join(" ");
+}
+export default toUpperAll;
